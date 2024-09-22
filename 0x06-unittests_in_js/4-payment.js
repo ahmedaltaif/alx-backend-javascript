0543fae8cfbe4686bd61704b4a21a984520b4811
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-09-22 04:31:10 +0300

    commit

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,14 +1,8 @@
-// Importing the Utils module
-const Utils = require('./utils');
+const Utils = require("./utils");
 
-// Function to send a payment request to the API
-const sendPaymentRequestToApi = (amount, shippingCost) => {
-  // Calculate the total cost by summing the amount and shipping cost using Utils.calculateNumber
-  const totalCost = Utils.calculateNumber('SUM', amount, shippingCost);
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    const result = Utils.calculateNumber("SUM", totalAmount, totalShipping);
+    console.log(`The total is: ${result}`);
+}
 
-  // Log the total cost to the console
-  console.log(`The total is: ${totalCost}`);
-};
-
-// Export the function to be used in other modules
 module.exports = sendPaymentRequestToApi;
